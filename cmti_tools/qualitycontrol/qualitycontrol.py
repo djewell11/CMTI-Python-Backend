06e3c4521fa1ca6c0542c4f12f943fe2e1798dda
bcman <bcmann@uwaterloo.ca>
2024-11-20 13:04:49 -0500

    Added data grading and wrote tests for it

--- cmti_tools/qualitycontrol/qualitycontrol.py
+++ cmti_tools/qualitycontrol/qualitycontrol.py
@@ -60,3 +60,143 @@ def check_units(value: int|float, expected_unit: str):
     return converted.magnitude
   except Exception as e:
     pass
+
+# Data Grading
+
+points_assignment = {
+    'Mine_Type': 3,
+    'Mine_Status': 4,
+    'Owner_Operator': 5,
+    'Dev_Stage': 3,
+    'Site_Access': 2,
+    'Orebody_Type': 1,
+    'Orebody_Class': 1,
+    'Ore_Minerals': 2,
+    'Ore_Processed': 4,
+    'Forcing_Features': 1,
+    'Raise_Type': 2,
+    'History_Stability_Concerns': 2,
+    'Acid_Generating': 2,
+    'Current_Max_Height': 4,
+    'Tailings_Storage_Method': 4,
+    'Tailings_Volume': 5,
+    'Tailings_Capacity': 5,
+    'Tailings_Area': 5
+  }
+
+comm_dict = {
+    'Commodity': 4,
+    'Commodity_Grade': 5,
+    'Commodity_Contained': 3,
+    'Commodity_Produced': 2
+  }
+
+year_dict = {
+    # Values for all keys should be the same
+    'Construction_Year': 3,
+    'Year_Opened': 3
+  }
+
+source_dict = {
+    'Source': 2,
+    'Source_Link': 3,
+    'Source_ID': 5
+  }
+
+class DataGrader:
+
+  def __init__(self, main:dict, comms:dict, years:dict, source:dict, comm_col_count=8, source_col_count=4):
+    self.main = main
+    self.comms = comms
+    self.commodity_cols = [f"Commodity{n}" for n in range(1, comm_col_count+1)]
+    self.years = years
+    self.source = source
+    self.source_col_count = source_col_count
+    self.source_cols = self.__create_source_columns()
+    self.perfect_score=0
+    self.perfect_score = self.__perfect_score()
+
+
+  # Init methods
+  def __create_source_columns(self):
+    source_cols = []
+    for source_n  in range(1, self.source_col_count+1):
+      source_cols += [f"Source_{source_n}", f"Source_{source_n}_ID", f"Source_{source_n}_Link"]
+      return source_cols
+
+  def perfect_row(self):
+    all_cols = {}
+    for col_dict in [self.main, self.comms, self.years, self.source]:
+      cols = col_dict.keys()
+      all_cols.update(dict(zip(cols, [True for n in range(len(cols))])))
+
+    full_row = pd.Series(all_cols)
+    return full_row
+
+  def __perfect_score(self):
+    perfect_score = 0
+    for point_dict in [self.main, self.comms, self.source]:
+      for points in point_dict.values():
+        perfect_score += points
+    return perfect_score
+
+  # Points calculation methods. These could be used individually, but it's easier to just call self.assign_score()
+
+  def calculate_main_values(self, row):
+    initial_points = 0
+    for key in self.main.keys():
+      entry = row[key]
+      if pd.notna(entry) and entry != 'Unknown':
+        initial_points += self.main[key]
+    return initial_points
+
+  def calculate_commodity_values(self, row):
+    comm_points = 0
+    cols_with_values = set()
+    for commodity_col in self.commodity_cols:
+      comm = row[commodity_col]
+      if pd.notna(comm):
+        comm = comm.strip()
+        cols_with_values.add('Commodity')
+        if f"{comm}_Produced" in row.index.tolist() and pd.notna(row[f"{comm}_Produced"]):
+          cols_with_values.add('Commodity_Produced')
+        if f"{comm}_Contained" in self.commodity_cols and pd.notna(row[f"{comm}_Contained"]):
+          cols_with_values.add('Commodity_Contained')
+        if f"{comm}_Grade" in self.commodity_cols and pd.notna(row[f"{comm}_Grade"]):
+          cols_with_values.add('Commodity_Grade')
+    for col in cols_with_values:
+      comm_points += comm_dict[col]
+    return comm_points
+
+  def calculate_year_values(self, row):
+    for col in self.years.keys():
+      col_value = row[col]
+      if pd.notnull(col_value) and col_value != 'Unknown':
+        # The first time a value is encountered, return value and exit method
+        year_points = self.years[col]
+        return year_points
+    return 0 # If no value encountered, no points returned
+
+  def calculate_source_values(self, row):
+    points_list = []
+    for i in range(1, self.source_col_count+1):
+      source_points = 0
+      if pd.notna(row[f'Source_{i}']):
+        if pd.notna(row[f'Source_{i}_ID']):
+          source_points += source_dict['Source_ID']
+        elif pd.notna(row[f'Source_{i}_Link']) and pd.isna(row[f'Source_{i}_ID']):
+          source_points += source_dict['Source_Link']
+        else:
+          source_points += source_dict['Source']
+      points_list.append(source_points)
+    return max(points_list)
+
+  def assign_score(self, row):
+    total_points = (
+        self.calculate_main_values(row) +
+        self.calculate_commodity_values(row) +
+        self.calculate_year_values(row) +
+        self.calculate_source_values(row)
+        )
+    score = round((total_points/self.perfect_score)*100, 2)
+    return score
--- tests/test_qualitycontrol.py
+++ tests/test_qualitycontrol.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
-from cmti_tools.qualitycontrol import check_categorical_values, check_units
+from cmti_tools.qualitycontrol import check_categorical_values, check_units, DataGrader
+from cmti_tools.qualitycontrol import points_assignment, comm_dict, year_dict, source_dict
 
 def test_categorical_vals(capfd):
     # Create a row with incorrect category vals
@@ -20,3 +21,42 @@ def test_check_units():
     converted = check_units(value, unit)
     # Check if units were properly converted, allowing for rounding error
     assert converted == pytest.approx(100000, 0.1)
+
+grader = DataGrader(points_assignment, comm_dict, year_dict, source_dict)
+
+def test_perfect_row():
+    row = grader.perfect_row
+
+    assert row['Mine_Type'] is True
+    assert row['Commodity'] is True
+    assert row['Construction_Year'] is True
+    assert row['Source'] is True
+
+def test_perfect_score():
+    assert grader.perfect_score == 85
+
+def test_assign_score():
+    row = pd.Series({
+        'Mine_Type': 'Underground',
+        'Mine_Status': 'Closed',
+        'Owner_Operator': None,
+        'Commodity1': 'Gold',
+        'Commodity1_Produced': 1000,
+        'Construction_Year': 'Unknown',
+        'Source_1': 'Source',
+        'Source_1_ID': None,
+        'Source_1_Link': 'https://...'
+    })
+
+    score = grader.assign_score(row)
+    expected_points = 3+4+4+2+2+3
+
+    expected_score = round((expected_points/grader.perfect_score)*100, 2)
+    assert score == expected_score
+
+def test_empty_row():
+    row = pd.Series({})
+
+    score = grader.assign_score(row)
+
+    assert score == 0
