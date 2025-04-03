af1a6e2b82ad5fed171608bc2f0b24402283478a
djewell11 <djewell11@gmail.com>
2025-04-03 16:10:14 -0300

    Remove commented-out code for Owner processing in db_to_dataframe for clarity

--- cmti_tools/export/export.py
+++ cmti_tools/export/export.py
@@ -131,12 +131,12 @@ def db_to_dataframe(worksheet:pd.DataFrame, session, name_convert_dict, method:L
           pass
       new_row = new_row | comms
 
-      # Owner
-      for owner in r.owners:
-        if owner.is_current_owner:
-          new_row['Owner_Operator'] = owner.name
-        else:
-          new_row['Past_Owner'] = owner.name
+      # # Owner
+      # for owner in r.owners:
+      #   if owner.is_current_owner:
+      #     new_row['Owner_Operator'] = owner.name
+      #   else:
+      #     new_row['Past_Owner'] = owner.name
 
       # Alias
       alias_list = []
